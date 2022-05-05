03c22fa32de10600b222ada5133044b5c529d9c7
Youssef ELMZELJI <youssef@brightleadsmedia.com>
2022-05-05 11:52:45 +0000

    Add Porject Configuration

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+//App Infos
+$port = 10178;
+$baseURL = 'http://localhost/';
+//Database Credentials
+$dbUsername = 'root';
+$dbPassword = 'root';
+$dbHost = 'localhost';
+$dbName = 'revpanda';
